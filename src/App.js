0bf0a045adf5d0086b8aee23a1e151d688abaf77
Selena X <selenaxx20@gmail.com>
2025-05-02 14:01:49 -0500

    added button to add images

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import searchAPI from './api';
 import ImageList from './components/ImageList';
 
 function App() {
-  const [images, setImages] = useState([]);
+	const [images, setImages] = useState([]);
+	const [count, setCount] = useState(0);
 
-  // received text from child component in input field
-  // pass to search api function, save json in result
-  const handleSearch = async (text) => {
-    const result = await searchAPI(text);
-    setImages(result);
-  };
+	// received text from child component in input field
+	// pass to search api function, save json in result
+	const handleSearch = async (text) => {
+		const result = await searchAPI(text);
+		setImages(result);
+	};
 
-  
-  return (
-    <div>
-      <SearchBar onSubmit={handleSearch} />
-      <ImageList images={images} />
-    </div>
-  );
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<SearchBar onSubmit={handleSearch} />
+			<ImageList images={images} />
+			<button onClick={handleClick}>add images</button>
+			<div>Numbers of images: {count}</div>
+		</div>
+	);
 }
 
 export default App;
